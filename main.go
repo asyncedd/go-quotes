5bb21fe1c6b080f6c9621c83079784d1bb9aa614
asyncedd <isynqquwu@proton.me>
2023-06-02 15:28:14 +0900

    style(gofumpt): 🎨 Style with Gofumpt
    CHANGES:
    - Fumpted via Gofumpt

--- main.go
+++ main.go
@@ -1,38 +1,36 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
+	"fmt"
+	"math/rand"
 )
 
 func randomInt(min, max int) int {
-  return min + rand.Intn(max - min)
+	return min + rand.Intn(max-min)
 }
 
 func main() {
-  var numberOfQuotes int
+	var numberOfQuotes int
 
-  var quotes = [...]string{
-    "yoooo",
-    "hi :3",
-    "Hello world!",
-  }
+	quotes := [...]string{
+		"yoooo",
+		"hi :3",
+		"Hello world!",
+	}
 
-  fmt.Printf("Enter the amount of quotes you want: ")
-  _, err := fmt.Scanln(&numberOfQuotes)
+	fmt.Printf("Enter the amount of quotes you want: ")
+	_, err := fmt.Scanln(&numberOfQuotes)
+	if err != nil {
+		fmt.Printf("Invalid input. Please enter a valid integer")
+		return
+	}
 
-  if err != nil {
-    fmt.Printf("Invalid input. Please enter a valid integer")
-    return
-  }
+	if numberOfQuotes < 0 {
+		fmt.Printf("Invalid number. Please enter a positive integer")
+		return
+	}
 
-  if numberOfQuotes < 0 {
-    fmt.Printf("Invalid number. Please enter a positive integer")
-    return
-  }
-
-
-  for i := 0; i < numberOfQuotes; i++ {
-    fmt.Printf("Your quote is: %s\n",  quotes[randomInt(0, len(quotes))])
-  }
+	for i := 0; i < numberOfQuotes; i++ {
+		fmt.Printf("Your quote is: %s\n", quotes[randomInt(0, len(quotes))])
+	}
 }
