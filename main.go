7729302ed203f9b597a14efa5718e6b29ec43400
asyncedd <isynqquwu@proton.me>
2023-06-02 16:55:13 +0900

    feat(quotes): ✨ Use the Zenquotes API
    This removes the need for maintaining a quotes list

--- main.go
+++ main.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
 )
+
+type Quote struct {
+	Text   string `json:"q"`
+	Author string `json:"a"`
+}
 
 func randomInt(min, max int) int {
 	return min + rand.Intn(max-min)
@@ -12,10 +19,10 @@ func randomInt(min, max int) int {
 func main() {
 	var numberOfQuotes int
 
-	quotes := [...]string{
-		"yoooo",
-		"hi :3",
-		"Hello world!",
+	quotes := []string{
+		"yoooo \n - Asyncedd",
+		"hi :3 \n - Also me",
+		"Hello world! \n - Some dude",
 	}
 
 	fmt.Printf("Enter the amount of quotes you want: ")
@@ -23,6 +30,25 @@ func main() {
 	if err != nil {
 		fmt.Printf("Invalid input. Please enter a valid integer")
 		return
+	}
+
+	resp, err := http.Get("https://zenquotes.io/api/quotes")
+	if err != nil {
+		fmt.Println("Error making request:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	var quotesAPI []Quote
+	err = json.NewDecoder(resp.Body).Decode(&quotesAPI)
+	if err != nil {
+		fmt.Println("Error decoding response:", err)
+		return
+	}
+
+	for _, q := range quotesAPI {
+		quote := fmt.Sprintf("%s \n - %s", q.Text, q.Author)
+		quotes = append(quotes, quote)
 	}
 
 	if numberOfQuotes < 0 {
