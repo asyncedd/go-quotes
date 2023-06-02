01feb4a8a4de8d92db867e134012c878c204a9e0
asyncedd <isynqquwu@proton.me>
2023-06-02 14:13:19 +0900

    feat(quotes): ✨ Make it generste a user generated thing

--- main.go
+++ main.go
@@ -10,11 +10,18 @@ func randomInt(min, max int) int {
 }
 
 func main() {
+  var numberOfQuotes int
+
   var quotes = [...]string{
     "yoooo",
     "hi :3",
     "Hello world!",
   }
 
-  fmt.Printf("Your quote is: %s\n",  quotes[randomInt(0, len(quotes))])
+  fmt.Printf("Enter the amount of quotes you want: ")
+  fmt.Scanln(&numberOfQuotes)
+
+  for i := 0; i < numberOfQuotes; i++ {
+    fmt.Printf("Your quote is: %s\n",  quotes[randomInt(0, len(quotes))])
+  }
 }
